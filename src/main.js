94405fad9ba7a9c3abb188b9e17bb27190666472
Bruno Marques <blmarques.dev@gmail.com>
2023-01-09 12:59:23 -0300

    feat(healt): create main app sample of healt check

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const teste = {
+  0: "teste",
+  1: "teste2",
+  2: "teste3",
+  3: "teste4",
+  4: "teste5",
+};
+
+console.log("length :", teste.length);
+// console.log("size :", teste.size());
